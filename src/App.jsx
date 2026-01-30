a3a7c28eb0411c801615b980236217c01207ecc5
amr ahmed <amr.ahmed20001451@gmail.com>
2026-01-30 17:09:57 +0200

    fixed the first reload first page window autofocus

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import RoomsSection from "./components/RoomFeatures";
-
+import { useEffect, useRef } from "react";
 
 
 function App() {
+
+  const mainRef = useRef(null);
+
+  useEffect(() => {    
+    if (mainRef.current) {
+      mainRef.current.focus();
+    }
+  }, []);
+
   return (
-    <main className="h-screen w-full overflow-y-scroll snap-y snap-mandatory scroll-smooth no-scrollbar bg-stone-900">
+    <main className="h-screen w-full overflow-y-scroll snap-y snap-mandatory scroll-smooth no-scrollbar bg-stone-900"
+    ref={mainRef}>
       {/* HERO */}
       <section className="h-screen w-full snap-start relative">
         <Hero />
